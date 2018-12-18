b05f399b7db25a5d760238aefa55a4c9f3cda992
raphaelnikson <nikson@fastdone.com.br>
2018-12-18 18:34:18 -0200

    Iniciando a tela de login

--- functions.php
+++ functions.php
@@ -1,6 +1,11 @@
 <?php
 
 /****************************************************************
-	Título
+	ASSETS
 ****************************************************************/
 
+function theme_assets(){
+	wp_enqueue_style( 'theme-stylesheet', get_stylesheet_uri(), false );
+}
+
+add_action('wp_enqueue_scripts', 'theme_assets');
---
+++ page-acessar-minha-conta.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+<section class="login-page">
+	<section class="col-text">
+		<section class="text">
+			<h1>Wp<strong>Tasks</strong></h1>
+			<p>Gerencie as tarefas da sua equipe de desenvolvimento.</p>
+		</section>
+	</section>
+	<section class="col-form">
+		<section class="form-box">
+			<h2>Bem vindo.</h2>
+			<p>Acesse sua conta do WpTasks ou <a href="#">crie uma nova conta</a></p>
+			<form>
+				<label>
+					<span>Nome de usuário:</span>
+					<input type="text">
+				</label>
+				<label>
+					<span>Senha:</span>
+					<input type="text">
+				</label>
+			</form>
+		</section>
+	</section>
+</section>
+<?php get_footer(); ?>
